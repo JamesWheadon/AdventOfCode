4d6a6e1132dadde0ed36b2ee578ae4e5947c2024
JamesWheadon <james.wheadon@yahoo.com>
2024-12-03 00:12:24 +0000

    fix bash script to generate new days

--- newDay.sh
+++ newDay.sh
@@ -1,23 +1,44 @@
-path="./src/main/kotlin"
-cd $path || exit
-if [ ! -d "$1" ]; then
-  mkdir "$1"
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <package_name>"
+    exit 1
 fi
-cd "./$1" || exit
-last_dir=$(find . -maxdepth 1 -type d -name "day*" | sort -n | tail -1)
-if [ -z "$last_dir" ]; then
-  next_dir="day01"
+
+PACKAGE_NAME=$1
+TEMPLATE_DIR="src/main/kotlin/dayTemplate"
+BASE_DIR="src/main/kotlin/$PACKAGE_NAME"
+
+if [ ! -d "$TEMPLATE_DIR" ]; then
+    echo "Error: Template directory $TEMPLATE_DIR does not exist."
+    exit 1
+fi
+
+mkdir -p "$BASE_DIR"
+
+LAST_DAY=$(find $BASE_DIR -maxdepth 1 -type d -name "day*" | sort -n | tail -1)
+if [ -z "$LAST_DAY" ]; then
+  NEXT_DAY="01"
 else
-  last_num=$(ls | grep -o 'day[0-9]*' | cut -c4- | sed 's/^0*//' | sort -n | tail -n 1)
-  next_num=$((last_num + 1))
-  next_dir="day$(printf '%02d' $next_num)"
+  LAST_NUM=$(ls $BASE_DIR | grep -o 'day[0-9]*' | cut -c4- | sed 's/^0*//' | sort -n | tail -n 1)
+  NEXT_DAY=$((LAST_NUM + 1))
+  NEXT_DAY="$(printf '%02d' $NEXT_DAY)"
 fi
-mkdir "$next_dir"
-day_num=$(echo "$next_dir" | grep -oE '[0-9]+')
-cp ../dayTemplate/Day00.kt day"${day_num}"/Day"${day_num}".kt
-cp ../dayTemplate/Day00_test.txt day"${day_num}"/Day"${day_num}"_test.txt
-cp ../dayTemplate/Day00.txt day"${day_num}"/Day"${day_num}".txt
-sed -i '' "s/package dayTemplate/package $1.day$day_num/g" day"${day_num}"/Day"${day_num}".kt
-sed -i '' "s/dayTemplate/$1\/day$day_num/g" day"${day_num}"/Day"${day_num}".kt
-sed -i '' "s/00/$day_num/g" day"${day_num}"/Day"${day_num}".kt
-open "day${day_num}/Day${day_num}.kt"
+
+NEW_DAY_DIR="$BASE_DIR/day$NEXT_DAY"
+
+cp -r "$TEMPLATE_DIR" "$NEW_DAY_DIR"
+
+find "$NEW_DAY_DIR" -type f -exec sed -i \
+    -e "s|/dayTemplate|/$PACKAGE_NAME/day$NEXT_DAY|g" \
+    -e "s|00|$NEXT_DAY|g" \
+    -e "s|package dayTemplate|package $PACKAGE_NAME.day$NEXT_DAY|g" {} \;
+
+find "$NEW_DAY_DIR" -type f -name "*Day00*" | while read -r FILE; do
+    NEW_NAME=$(echo "$FILE" | sed "s|Day00|Day$NEXT_DAY|g")
+    mv "$FILE" "$NEW_NAME"
+done
+
+echo "New day package created at: $NEW_DAY_DIR"
